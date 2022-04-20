e68f8e3edbb88620d9b7f22fbb4c4bbd65986fd4
JakobDucharme <100697023+JakobDucharme@users.noreply.github.com>
2022-04-19 22:00:14 -0400

    Delete Cave.tsx

--- Cave.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.8" tiledversion="1.8.4" name="Cave" tilewidth="32" tileheight="32" tilecount="728" columns="8">
- <image source="Desktop/Project1/Graphics/Tilesets/tileset_cave.png" width="256" height="2912"/>
-</tileset>
